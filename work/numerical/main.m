56793ac908e17e8c0ddda1111e6e9dc3c4d56c70
David Meissner <dmeissner@forestconcepts.com>
2021-06-09 18:40:57 -0700

    Working version that is the beginning of adding water vapor to the mix. ;P

--- work/numerical/main.m
+++ work/numerical/main.m
@@ -1,8 +1,9 @@
 clear
 close all
 clc
-
-T_g = arda_numerical();
-
+% function [T_g, T_h, P_h] = arda_numerical(T_g0, delta_T)
+[T_g, T_h, P_h] = arda_numerical(20, 100);
+fprintf('Max heater output (W): %f\n', max(P_h))
+fprintf('Min heater output (W): %f\n', min(P_h))
 
 
