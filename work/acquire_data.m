1e9e2a5227bd6eab1e44ce9e988cb411e996cab4
David Meissner <dmeissner@forestconcepts.com>
2021-06-17 14:53:39 -0700

    Editing acquire_data.m with filtering function.

--- work/acquire_data.m
+++ work/acquire_data.m
@@ -1,86 +1,118 @@
-clearvars -except thermocouple_fit_obj
-close all; home
+function acquire_data()
 %% Acquire data from Measurement Computing DAQ
 
 % Create a daq object
-daq_obj = daq("mcc");
+d = daq("mcc");
 
 % Set the sampling frequency
-daq_obj.Rate = 10;
+d.Rate = 10;
 
 % Add a channel for the J type thermocouple connected to A4
-daq_obj.addinput("Board1", "Ai4", "Voltage")
+d.addinput("Board1", "Ai4", "Voltage") % above heating element in plastic
+d.addinput("Board1", "Ai6", "Voltage") % jammed in heating element
+
 
 %%
 % Set the amount of time to acquire data
-acquisition_window = 30; % seconds
+acquisition_window = 10; % seconds
 
 % Create a dialog box for the user
 dialog_figure = uifigure('HandleVisibility', 'on');
 dialog_box = uiprogressdlg(dialog_figure, "Title", "Acquiring Data", "Message",...
     strcat(...
-        strcat(...
-            strcat('Started: ', string(datetime('now')))...
-            , '  End: ')...
-        , string(datetime('now') + duration(0,0, acquisition_window))));
+    strcat(...
+    strcat('Started: ', string(datetime('now')))...
+    , '  End: ')...
+    , string(datetime('now') + duration(0,0, acquisition_window))));
 drawnow
 
 %% Read the data
-[data, start_time] = read(daq_obj, seconds(acquisition_window));
+[data, ~] = read(d, seconds(acquisition_window));
 
 % Smooth and convert from V to mV
-%y_s = smooth(data.Board1_Ai4, 100); 
-y_s = data.Board1_Ai4 * 1000; % convert to mV
+%y_s = smooth(data.Board1_Ai4, 100);
+y_A4 = data.Board1_Ai4 * 1000; % convert to mV
+y_A6 = data.Board1_Ai6 * 1000; % convert to mV
 
 %% Create the fit object needed to convert mV to C
 if ~exist('thermocouple_fit_obj','var')
-    [thermocouple_fit_obj, gof] = create_fit_obj_volts2celsius();
+    [thermocouple_fit_obj, ~] = create_fit_obj_volts2celsius();
 end
 
 %% Convert to C and filter
 reference = 27;
 
 % Convert to C and shift up by the reference
-y_C = thermocouple_fit_obj(y_s) + reference;
+y_C4 = thermocouple_fit_obj(y_A4) + reference;
+y_C6 = thermocouple_fit_obj(y_A6) + reference;
 
 % Apply noise filter
-y_C2 = filter_stuff(y_C, daq_obj.Rate);
+y_C4_filtered = filter_stuff(y_C4, d.Rate);
+y_C6_filtered = filter_stuff(y_C6, d.Rate);
 
 % Get the mean
-C = mean(y_C);
+C4 = mean(y_C4_filtered);
+C6 = mean(y_C6_filtered);
 
 %% Convert to Fahrenheit
 
 % Convert C to F
-y_F = y_C*9./5 + 32;
+y_F4 = y_C4*9./5 + 32;
+y_F6 = y_C6*9./5 + 32;
 
 % Apply noise filter
-y_F2 = filter_stuff(y_F, daq_obj.Rate);
+y_F4_filtered = filter_stuff(y_F4, d.Rate);
+y_F6_filtered = filter_stuff(y_F6, d.Rate);
 
 % Get the mean
-F = mean(y_F2);
+F4 = mean(y_F4_filtered);
+F6 = mean(y_F6_filtered);
 
 %% Plot
 
 % Raw data
-figure; plot(data.Board1_Ai4); grid on; 
+figure
+title('Raw Data')
+subplot(2,1,1); plot(data.Board1_Ai4); grid on; ylabel('A4')
+subplot(2,1,2); plot(data.Board1_Ai6); grid on; ylabel('A6')
+xlabel('Time')
 
+%%
 % Celsius
-figure; subplot(2,1,1); 
-plot(y_C, 'Color', '#0072BD'); grid on; hold on  % blue
-plot(y_C2, 'Color', '#D95319')  % orange
+figure;
+subplot(2,1,1);
+plot(y_C4, 'Color', '#0072BD'); grid on; hold on  % blue
+plot(y_C4_filtered, 'Color', '#D95319')  % orange
 ylabel('Temp. (C)')
-axis([0 numel(data.Variables) C-6 C+6])
+axis([0 numel(data.Variables)/2 C4-6 C4+6])
+title('Temp of A4 (Other one)')
 
 % Fahrenheit
-
 subplot(2,1,2)
-plot(y_F, 'Color', '#0072BD'); grid on; hold on  % blue
-plot(y_F2, 'Color', '#D95319')  % orange
+plot(y_F4, 'Color', '#0072BD'); grid on; hold on  % blue
+plot(y_F4_filtered, 'Color', '#D95319')  % orange
 ylabel('Temp. (F)')
-axis([0 numel(data.Variables) F-10 F+10])
+axis([0 numel(data.Variables)/2 F4-10 F4+10])
 
-fprintf('Average Temperature: %.1f C (%.1f F)\n', C, F)
+%%
+% Celsius
+figure;
+subplot(2,1,1);
+plot(y_C6, 'Color', '#0072BD'); grid on; hold on  % blue
+plot(y_C6_filtered, 'Color', '#D95319')  % orange
+ylabel('Temp. (C)')
+axis([0 numel(data.Variables)/2 C6-6 C6+6])
+title('Temp of A6 (Little one)')
+
+% Fahrenheit
+subplot(2,1,2)
+plot(y_F6, 'Color', '#0072BD'); grid on; hold on  % blue
+plot(y_F6_filtered, 'Color', '#D95319')  % orange
+ylabel('Temp. (F)')
+axis([0 numel(data.Variables)/2 F6-10 F6+10])
+
+fprintf('Average Temperature A4: %.1f C (%.1f F)\n', C4, F4)
+fprintf('Average Temperature A6: %.1f C (%.1f F)\n', C6, F6)
 try
     close(dialog_box)
     close(dialog_figure)
@@ -89,3 +121,82 @@ end
 
 % Save all variables
 save_everything(true, 'daq_results')
+
+
+    function save_everything(varargin)
+        
+        if nargin ~= 2
+            error('The function save_everything.m requires 2 inputs.')
+        else
+            if islogical(varargin{1}) && size(varargin{1},1) == 1 ...
+                    && size(varargin{1},2) == 1
+                
+                save_stuff = varargin{1};
+            else
+                error('First argument must be a logical datatype of size 1x1.')
+            end
+            
+            if ischar(varargin{2})
+                folder_name = varargin{2};
+            else
+                error('Second argument must be a char datatype.')
+            end
+        end
+        
+        if save_stuff
+            try
+                working_folder = pwd;
+                dir_info = dir;
+                make_folder = true;
+                
+                for i = 1:length(dir_info)
+                    if strcmpi(dir_info(i).name, folder_name)
+                        make_folder = false;
+                        break
+                    end
+                end
+                
+                if make_folder
+                    mkdir(folder_name)
+                end
+                
+                datetime_str = datestr(now,'yyyy-mm-dd_HHMMSS');
+                
+                variables_filename = sprintf([folder_name '_%s'], ...
+                    datetime_str);
+                save(fullfile(working_folder, folder_name, ...
+                    variables_filename));
+                
+            catch
+                warning('Unable to save simulation results.')
+            end
+        end
+        
+    end
+
+end
+%%
+function y_out = filter_stuff(y_in, sample_frequency)
+% Setup for filter()
+windowSize = 20*sample_frequency;
+b = (1/windowSize)*ones(1,windowSize);
+a = 1;
+
+% Filter with base matlab filter
+y2 = filter(b, a, y_in);
+
+% Flip to prep for the 2nd filtering
+y_out = zeros(size(y_in));
+for i = 1:samples
+    y_out(i) = y2(end-i+1);
+end
+
+% Filter the flipped data to remove lateral offset
+ynew = filter(b,a,y_out);
+
+% Flip the data again so it's initial orientation
+for i = 1:samples
+    y_out(i) = ynew(end-i+1);
+end
+
+end
--- work/numerical/arda_numerical.m
+++ work/numerical/arda_numerical.m
@@ -14,108 +14,7 @@ function [T_g_out, T_h_out, P_h_out, phi_out] = arda_numerical(delta_T, T_o0, ph
 %
 % Example:
 %   [T_g, T_h, P_h] = arda_numerical(20, 100, 20, 0.50, 300)
-
-%% Progress dialog
-fig = uifigure('HandleVisibility', 'on');
-d = uiprogressdlg(fig, "Title", "MATLAB is thinking", "Message",...
-    "Starting...", "Cancelable", "on");%, "Indeterminate", "on");
-drawnow
-
-%% Convert to Kelvin
-c2k = 273.15;
-T_g0 = 20;
-T_g0 = T_g0 + c2k;
-T_o0 = T_o0 + c2k;
-T_h0 = T_h0 + c2k;
-
-%% Define system constants
-R_a = 287.058;      % J/(kg*K), Specific Gas Constant, dry air
-R_v = 461.495;      % J/(kg*K), Specific Gas Constant, water vapor
-R_go = 0.03;        % K/W, Thermal resistance of the gas mixture to outside interface (1 inch insulation)
-R_hg = 0.005;       % K/W, Thermal resistance of the heater to gax mixture interface
-constants = [R_a, R_v, R_go, R_hg];
-
-% Create a vector for specific heat of air
-% Ref: https://www.me.psu.edu/cimbala/me433/Links/Table_A_9_CC_Properties_of_Air.pdf
-if exist('specific_heat_table.mat', 'file')
-    load('specific_heat_table.mat','specific_heat_fit_obj');
-else
-    [specific_heat_fit_obj, ~] = calculate_specific_heat_data();
-end
-
-
-p_g0 = 101.325;     % kPa, Initial pressure of the control volume.
-V_g0 = 0.25;        % m^3, Initial volume of aRDA "control volume" (constant)
-
-% Calculate initial saturation pressure (needed for humidity ratio)
-p_s0 = calculate_saturation_pressure(T_g0);
-
-% kg/m^3, density of dry air at 20C and 1 atm
-rho_a0 = XDryAir('tK', T_g0, 'rho', p_g0); 
-
-% kg, mass of dry air
-m_a0 = rho_a0 * V_g0;
-
-% kg, mass of vapor
-m_v0 = m_a0 * phi_0 * 0.622 * (p_s0 / (p_g0 - p_s0)); 
-
-% Initial humidity ratio
-% m_v0 = phi_0 * p_s0 * V_g0 / (R_v * T_g0);
-q_0 = m_v0 / (m_a0 + m_v0); 
-
-% kPa, Initial partial pressure of water vapor
-p_v0 = q_0 * p_g0/(q_0 + 0.622);
-
-% kPa, Initial partial pressure of air
-p_a0 = p_g0 - p_v0;
-
-% Initial state vector
-x_0 = [T_g0; T_h0; T_o0; p_g0; p_v0; p_a0; V_g0; q_0; phi_0; p_s0; m_v0; m_a0];
-
-
-%% Define the temperature controller
-% Initialize the controller
-pid = controller_pid;
-
-% Set the time step
-pid.Ts = T_s;
-
-tau = 2532;
-tau_dead = 10; 
-P = (197.1 - 97.1)/97.1; % Percent change of input
-N = ((25.81 - 22.87)/22.87) / tau; % Percent change of output / tau
-L = tau_dead;
-R = tau_dead / tau;
-K_0 = P/(N*L);
-
-% Cohen-Coon 1(only for self-regulating processes, like flow, temp, pressure)
-pid.Kp = K_0 * (1.33 + R/4);
-pid.Ki = (L * (30 + 3*R) / (9 + 20 * R));
-pid.Kd = (4 * L / (11 + 2 * R));
-pid.tau = tau;
-pid.min_output = 0;
-pid.max_output = 11000; % Max watts from heater
-
-%% Initialize the relative humidity controller
-
-pid_phi = controller_pid;
-pid_phi.Ts = pid.Ts;
-
-pid_phi.tau = 59;
-P2 = (0.1001 - 0.001)/0.001;
-N2 = ((0.3 - 0.2)/0.2) / pid_phi.tau;
-L2 = 0.1; % tau_dead
-R2 = tau_dead / pid_phi.tau;
-K_02 = P2 / (N2 * L2);
-
-pid_phi.Kp = K_02 * (1.33 + R2/4);
-pid_phi.Ki = (L2 * (30 + 3*R2) / (9 + 20 * R2));
-pid_phi.Kd = (4 * L2 / (11 + 2 * R2));
-pid_phi.min_output = 0;
-pid_phi.max_output = 1;
-
-measurement = x_0(1);
-measurement_phi = x_0(9);
+%%
 T_g_out = zeros(max_iterations, 1);
 T_h_out = zeros(max_iterations, 1);
 T_o_out = zeros(max_iterations, 1);
@@ -126,80 +25,212 @@ phi_out = zeros(max_iterations, 1);
 p_s_out = zeros(max_iterations, 1);
 P_h_out = zeros(max_iterations, 1);
 m_steam = zeros(max_iterations, 1);
-
-d.Message = "Processing...";
-drawnow
+x_0 = zeros(12,1);
+constants = zeros(1,4);
+specific_heat_fit_obj = [];
 draw_times = 1:10:max_iterations;
 
+[fig, d] = progress_dialog();
+T_g0 = 20;
+set_initial_conditions()
+define_constants()
+x_0 = set_initial_conditions();
+pid = controller_pid;
+pid_phi = controller_pid;
+define_temperature_controller()
+define_relative_humidity_controller()
+setup_progress_dialog()
+
 setpoint_T = T_g0 + delta_T;
-setpoint_phi = 0.5;
 
+main_loop()
 
-%%
-for i = 1:max_iterations
-    %% Controller 
-    % Calculate controller output
-    pid = calculate_controller_output(pid, setpoint_T, measurement);
-    if i == 138
-        keyboard
-    end
-    pid_phi = calculate_controller_output(pid_phi, setpoint_phi, ...
-        measurement_phi);
-    
-    % Record the power setting of the heater at each time step
-    P_h_out(i) = pid.out;
-    m_steam(i) = pid_phi.out;
-    
-    %% Plant Model
-    % On the first time through the loop, use the x_0 above. After that,
-    % the initial values to give to the ode are the last ones it gave.
-    if i >= 2
-        x_0 = y(end,:);
-    end
-
-    % Call ode solver 
-    [~, y] = ode23(@(t,y) odefun(t, y, pid, pid_phi, constants,...
-        specific_heat_fit_obj, i), [0 pid.Ts], x_0);
-    if any(isnan(y))
-        keyboard
-    end
-    % Capture sensor measurements
-    measurement = y(end,1);
-    measurement_phi = y(end,9);
-    T_g_out(i) = y(end,1);
-    T_h_out(i) = y(end,2);
-    T_o_out(i) = y(end,3);
-    p_v_out(i) = y(end,5);
-    p_a_out(i) = y(end,6);
-    q_out(i)   = y(end,8);
-    phi_out(i) = y(end,9);
-    p_s_out(i) = y(end,10);
-       
-    if ismember(i, draw_times)
-        d.Value = i/max_iterations;
-        drawnow
-        if d.CancelRequested
-            break
-        end
-    end
-end
-
-d.Message = 'Plotting...';
-drawnow
-
-%% Convert to Celsius
-T_g_out = T_g_out - c2k;
-T_h_out = T_h_out - c2k;
-T_o_out = T_o_out - c2k;
-setpoint_T = T_g0 + delta_T;
-setpoint_T = setpoint_T - c2k;
+convert_to_C()
 
 create_plots(T_g_out, T_h_out, T_o_out, p_v_out, p_a_out, q_out,...
     phi_out, p_s_out, P_h_out, setpoint_T, max_iterations, m_steam)
 
-% Done with progress dialog
-close(d)
-close(fig)
+close(d); close(fig)
+
+%% Progress dialog
+    function [fig, d] = progress_dialog()
+        fig = uifigure('HandleVisibility', 'on');
+        d = uiprogressdlg(fig, "Title", "MATLAB is thinking", "Message",...
+            "Starting...", "Cancelable", "on");%, "Indeterminate", "on");
+        drawnow
+    end
+
+%% Define constants
+    function define_constants()
+        R_a = 287.058;      % J/(kg*K), Specific Gas Constant, dry air
+        R_v = 461.495;      % J/(kg*K), Specific Gas Constant, water vapor
+        R_go = 0.03;        % K/W, Thermal resistance of the gas mixture to outside interface (1 inch insulation)
+        R_hg = 0.005;       % K/W, Thermal resistance of the heater to gax mixture interface
+        constants = [R_a, R_v, R_go, R_hg];
+        
+        % Create a vector for specific heat of air
+        % Ref: https://www.me.psu.edu/cimbala/me433/Links/Table_A_9_CC_Properties_of_Air.pdf
+        if exist('specific_heat_table.mat', 'file')
+            load('specific_heat_table.mat','specific_heat_fit_obj');
+        else
+            [specific_heat_fit_obj, ~] = calculate_specific_heat_data();
+        end
+        
+    end
+
+%% Set initial conditions
+function x_0 = set_initial_conditions()
+        c2k = 273.15;
+        
+        T_g0 = T_g0 + c2k;
+        T_o0 = T_o0 + c2k;
+        T_h0 = T_h0 + c2k;
+        p_g0 = 101.325;     % kPa, Initial pressure of the control volume.
+        V_g0 = 0.25;        % m^3, Initial volume of aRDA "control volume" (constant)
+        
+        % Calculate initial saturation pressure (needed for humidity ratio)
+        p_s0 = calculate_saturation_pressure(T_g0);
+        
+        % kg/m^3, density of dry air at 20C and 1 atm
+        rho_a0 = XDryAir('tK', T_g0, 'rho', p_g0);
+        
+        % kg, mass of dry air
+        m_a0 = rho_a0 * V_g0;
+        
+        % kg, mass of vapor
+        m_v0 = m_a0 * phi_0 * 0.622 * (p_s0 / (p_g0 - p_s0));
+        
+        % Initial humidity ratio
+        % m_v0 = phi_0 * p_s0 * V_g0 / (R_v * T_g0);
+        q_0 = m_v0 / (m_a0 + m_v0);
+        
+        % kPa, Initial partial pressure of water vapor
+        p_v0 = q_0 * p_g0/(q_0 + 0.622);
+        
+        % kPa, Initial partial pressure of air
+        p_a0 = p_g0 - p_v0;
+        
+        % Initial state vector
+        x_0 = [T_g0; T_h0; T_o0; p_g0; p_v0; p_a0; V_g0; q_0; phi_0; p_s0; m_v0; m_a0];
+    end
+
+%% Define the temperature controller
+    function define_temperature_controller()
+       
+        
+        % Set the time step
+        pid.Ts = T_s;
+        
+        tau = 2532;
+        tau_dead = 10;
+        P = (197.1 - 97.1)/97.1; % Percent change of input
+        N = ((25.81 - 22.87)/22.87) / tau; % Percent change of output / tau
+        L = tau_dead;
+        R = tau_dead / tau;
+        K_0 = P/(N*L);
+        
+        % Cohen-Coon 1(only for self-regulating processes, like flow, temp, pressure)
+        pid.Kp = K_0 * (1.33 + R/4);
+        pid.Ki = (L * (30 + 3*R) / (9 + 20 * R));
+        pid.Kd = (4 * L / (11 + 2 * R));
+        pid.tau = tau;
+        pid.min_output = 0;
+        pid.max_output = 11000; % Max watts from heater
+    end
+
+%% Initialize the relative humidity controller
+    function define_relative_humidity_controller()
+        
+        pid_phi.Ts = pid.Ts;
+        pid_phi.tau = 59;
+        tau_dead = 0.1;
+        P2 = (0.1001 - 0.001)/0.001;
+        N2 = ((0.3 - 0.2)/0.2) / pid_phi.tau;
+        L2 = 0.1; % tau_dead
+        R2 = tau_dead / pid_phi.tau;
+        K_02 = P2 / (N2 * L2);
+        
+        pid_phi.Kp = K_02 * (1.33 + R2/4);
+        pid_phi.Ki = (L2 * (30 + 3*R2) / (9 + 20 * R2));
+        pid_phi.Kd = (4 * L2 / (11 + 2 * R2));
+        pid_phi.min_output = 0;
+        pid_phi.max_output = 1;
+    end
+
+%% Setup progress dialog
+    function setup_progress_dialog()
+        d.Message = "Processing...";
+        drawnow
+    end
+
+%% Main loop
+    function main_loop()
+        measurement = x_0(1);
+        measurement_phi = x_0(9);
+        
+        setpoint_phi = 0.5;
+        %%
+        for i = 1:max_iterations
+            %% Controller
+            % Calculate controller output
+            pid = calculate_controller_output(pid, setpoint_T, measurement);
+            if i == 138
+                keyboard
+            end
+            pid_phi = calculate_controller_output(pid_phi, setpoint_phi, ...
+                measurement_phi);
+            
+            % Record the power setting of the heater at each time step
+            P_h_out(i) = pid.out;
+            m_steam(i) = pid_phi.out;
+            
+            %% Plant Model
+            % On the first time through the loop, use the x_0 above. After that,
+            % the initial values to give to the ode are the last ones it gave.
+            if i >= 2
+                x_0 = y(end,:);
+            end
+            
+            % Call ode solver
+            [~, y] = ode23(@(t,y) odefun(t, y, pid, pid_phi, constants,...
+                specific_heat_fit_obj, i), [0 pid.Ts], x_0);
+            if any(isnan(y))
+                keyboard
+            end
+            % Capture sensor measurements
+            measurement = y(end,1);
+            measurement_phi = y(end,9);
+            T_g_out(i) = y(end,1);
+            T_h_out(i) = y(end,2);
+            T_o_out(i) = y(end,3);
+            p_v_out(i) = y(end,5);
+            p_a_out(i) = y(end,6);
+            q_out(i)   = y(end,8);
+            phi_out(i) = y(end,9);
+            p_s_out(i) = y(end,10);
+            
+            if ismember(i, draw_times)
+                d.Value = i/max_iterations;
+                drawnow
+                if d.CancelRequested
+                    break
+                end
+            end
+            
+            pause(1)
+            
+        end
+    end
+
+%% Convert to Celsius
+    function convert_to_C()
+        T_g_out = T_g_out - c2k;
+        T_h_out = T_h_out - c2k;
+        T_o_out = T_o_out - c2k;
+        setpoint_T = T_g0 + delta_T;
+        setpoint_T = setpoint_T - c2k;
+    end
+
 end
 
 %% ODE of thermal system
@@ -210,7 +241,7 @@ p_v_dot = 0;
 p_a_dot = 0;
 V_g_dot = 0;
 
-    
+
 % System variables
 T_g = x(1); % T_g0  1) K, initial T_g
 T_h = x(2); % T_h0; 2) K, Initial heater temp, T_h
@@ -225,7 +256,7 @@ p_s = x(10); % p_s0];10) Initial saturation vapor pressure
 m_v = x(11);
 m_a = x(12);
 
-% Constants       
+% Constants
 C_g = fit_obj(T_g); % J/(kg*K), Specific heat capacity of
 C_h = 4660;         % J/(kg*K), Specific heat capacity of the heater
 c_p = 0.718;        % Specific heat of air at constant pressure
@@ -248,7 +279,7 @@ V_g2 = V_g + delta(7);
 
 %% Change in m_a
 % kg/m^3, density of dry air at 20C and 1 atm
-rho_a = XDryAir('tK', T_g, 'rho'); 
+rho_a = XDryAir('tK', T_g, 'rho');
 
 % kg, mass of dry air
 m_a2 = rho_a * V_g2;
@@ -283,8 +314,8 @@ delta(11) = (m_v2 - m_v) + pid_phi.out; % kg per time step
 
 %% dT_h/dt
 delta(2) = x(1)/(C_h * m_h * R_hg)...
-         - x(2)/(C_h * m_h * R_hg)...
-         + pid.out/(C_h * m_h) ;
+    - x(2)/(C_h * m_h * R_hg)...
+    + pid.out/(C_h * m_h) ;
 % T_h2 = T_h + delta(2);
 
 %% dp_s/dt (change in saturation pressure)
@@ -292,7 +323,7 @@ p_s2 = calculate_saturation_pressure(T_g2);
 delta(10) = p_s2 - p_s;
 
 %% dq/dt (change in humidity ratio)
-q2 = m_v2 / (m_a2 + m_v2); 
+q2 = m_v2 / (m_a2 + m_v2);
 delta(8) = q2 - q;
 
 %% dp_v/dt
